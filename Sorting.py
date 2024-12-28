302a0c7a0505546266055f31260b1c9bbebdb97f
SHITOL-CSE-RU <shitol.cse@gmail.com>
2024-12-28 14:00:13 +0600

    Indexing and sorting

---
+++ Sorting.py
@@ -0,0 +1,7 @@
+# লিস্টে সর্টিং
+my_list = [50, 20, 40, 10, 30]
+my_list.sort()  # Ascending order (বৃদ্ধিমূলক)
+print(my_list)  # Output: [10, 20, 30, 40, 50]
+
+my_list.sort(reverse=True)  # Descending order (হ্রাসমূলক)
+print(my_list)  # Output: [50, 40, 30, 20, 10]
---
+++ indexing.py
@@ -0,0 +1,8 @@
+# লিস্টে ইন্ডেক্সিং
+my_list = [10, 20, 30, 40, 50]
+print(my_list[0])  # Output: 10 (0 ইন্ডেক্সে 10 আছে)
+print(my_list[2])  # Output: 30 (2 ইন্ডেক্সে 30 আছে)
+
+# স্ট্রিংয়ে ইন্ডেক্সিং
+my_string = "Hello"
+print(my_string[1])  # Output: e (1 ইন্ডেক্সে e আছে)
